9a27cef7e86709f1389a37caf4b9ddfefafe8283
xconstruct <xconstruct@gmail.com>
2009-10-28 15:04:29 +0100

    less bgs is actually a good thing

--- infos/xp.lua
+++ infos/xp.lua
@@ -5,6 +5,6 @@ OhNoesQueues:RegisterInfoText("BG XP needed", function(button, text)
 	if(not xp) then return text:SetText("") end
 
 	local forLevel = (UnitXPMax("player")-UnitXP("player")) / xp / LPVP.XP_FACTOR
-	local r,g,b = OhNoesQueues.ColorGradient(forLevel/15, 1,0,0, 1,1,0, 0,1,0)
+	local r,g,b = OhNoesQueues.ColorGradient(forLevel/20, 0,1,0, 1,1,0, 1,0,0)
 	text:SetFormattedText("|cff%2x%2x%2x%.1f|r", r*255,g*255,b*255, forLevel)
 end)
